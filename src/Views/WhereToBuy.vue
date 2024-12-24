23b239c7f440539fb6ce4c09a5191cfeb5451e8d
Elijan Mastnak <git@ejmastnak.com>
2024-12-24 15:56:25 +0100

    wip: add WhereToBuy page
    Creates WhereToBuy page, currently with placeholder/outlined content
    but with underlying page structure defined.

--- src/Views/WhereToBuy.vue
+++ src/Views/WhereToBuy.vue
@@ -1,12 +1,37 @@
 <script setup>
 import I18nRouterLink from '@/Components/I18nRouterLink.vue'
 import PageWrapper from '@/Shared/PageWrapper.vue'
+import { ChevronRightIcon } from '@heroicons/vue/24/outline'
+import Disclosure from '@/Components/Disclosure.vue'
 </script>
 
 <template>
 
   <PageWrapper>
-    <h1 class="text-5xl font-['Latin_Modern_Roman']">Where to buy</h1>
+    <h1 class="text-5xl font-['Latin_Modern_Roman']">{{$t('wheretobuy.h1')}}</h1>
+
+    <div class="mt-8">
+      {{$t('wheretobuy.intro')}}
+      <ul class="mt-3 space-y-2">
+        <li>
+          <Disclosure :summary="$t('wheretobuy.waystobuy.farm.summary')" >
+            {{$t('wheretobuy.waystobuy.farm.details')}}
+          </Disclosure>
+        </li>
+        <li>
+          <Disclosure :summary="$t('wheretobuy.waystobuy.ljubljana.summary')" >
+            {{$t('wheretobuy.waystobuy.ljubljana.details')}}
+          </Disclosure>
+        </li>
+      </ul>
+
+      <div class="mt-5 max-w-xl">
+        <p>
+          <strong class="font-bold">{{$t('wheretobuy.waystobuy.largeOrders.summary')}}</strong> {{$t('wheretobuy.waystobuy.largeOrders.details')}}
+        </p>
+      </div>
+
+    </div>
   </PageWrapper>
 
 </template>
--- src/i18n/translations/wheretobuy.js
+++ src/i18n/translations/wheretobuy.js
@@ -1,10 +1,47 @@
 export default {
   h1: {
     en: "Where to buy",
+    sl: "Kje kupiti",
+  },
+  intro: {
+    en: "There are two ways to get our products at the moment:",
     sl: "Nekaj po slovensko",
   },
-  p: {
-    en: "Something in English.",
-    sl: "Nekaj po slovensko",
-  },
+  waystobuy: {
+    farm: {
+      summary: {
+        en: "Buy on our farm in Tolsti vrh pri Mislinji",
+        sl: "Nekaj po slovensko",
+      },
+      details: {
+        en: `Come to our farm in person.
+Tolsti vrh pri Mislinji 47, Tolsti vrh pri Mislinji, 2382 Milsinja.
+Contact us to arrange pick-up, and we'll have your order ready on location.
+Look around, get discounted prices, try samples of our products, etc.`,
+        sl: "Nekaj po slovensko",
+      }
+    },
+    ljubljana: {
+      summary: {
+        en: "Pick up in Ljubljana, Center on Saturdays",
+        sl: "Nekaj po slovensko",
+      },
+      details: {
+        en: `Elijan works part-time in Ljubljana and can arrange delivery to Ljubljana on Saturdays (or other days by agreement).
+            Contact us to arrange an order and pick-up time.
+            You would pick up near Župančičeva ulica in Ljubljana, or we could arrange home delivery to locations in the Ljubljana area.`,
+        sl: "Nekaj po slovensko",
+      }
+    },
+    largeOrders: {
+      summary: {
+        en: "Large orders:",
+        sl: "Nekaj po slovensko",
+      },
+      details: {
+        en: `for larger orders we can deliver to your location in Slovenia. Contact us to arrange details. `,
+        sl: "Nekaj po slovensko",
+      }
+    },
+  }
 }
