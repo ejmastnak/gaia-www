c8ed20ac09cbf50bafd12e9058f44da334590c4e
Elijan Mastnak <git@ejmastnak.com>
2025-01-05 10:56:16 +0100

    style: remove hardcoded blue hover link text

--- src/Components/I18nRouterLink.vue
+++ src/Components/I18nRouterLink.vue
@@ -12,7 +12,7 @@ const props = defineProps({
 
 <template>
   <RouterLink
-    class="p-px rounded-md hover:text-blue-700 hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-700"
+    class="p-px rounded-md hover:underline focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-700"
     :class="{ 'text-blue-600': colored }"
     :to="{name: routeBasename + '.' + Translate.currentLocale}"
   >
