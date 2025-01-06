9f27261a108c4ebdb78bf67efb1cbc63170f5115
Elijan Mastnak <git@ejmastnak.com>
2025-01-06 18:57:19 +0100

    feat: add image to Contact page

--- src/Views/Contact/Contact.vue
+++ src/Views/Contact/Contact.vue
@@ -4,6 +4,7 @@ import I18nRouterLink from '@/Components/I18nRouterLink.vue'
 import PageWrapper from '@/Shared/PageWrapper.vue'
 import Disclosure from '@/Components/Disclosure.vue'
 import ContactForm from './Partials/ContactForm.vue'
+import appleBlossomsImage from '@/assets/img/apple-blossoms.jpg'
 </script>
 
 <template>
@@ -11,55 +12,66 @@ import ContactForm from './Partials/ContactForm.vue'
   <PageWrapper>
     <h1 class="text-5xl font-['Latin_Modern_Roman']">{{$t('contact.h1')}}</h1>
 
-    <p class="mt-10 max-w-2xl">
-      {{$t('contact.subheading')}}
-    </p>
+    <div class="mt-10 lg:flex space-x-16 lg:-mr-12">
+      <div>
+        <p class="max-w-2xl">
+          {{$t('contact.subheading')}}
+        </p>
 
-    <p class="mt-5">
-      {{$t('contact.waysToContactUs')}}
-    </p>
+        <p class="mt-5">
+          {{$t('contact.waysToContactUs')}}
+        </p>
 
-    <ul class="mt-3 space-y-2">
-      <li>
-        <Disclosure>
-          <template #summary>
-            <span>{{$t('contact.waysToContact.phone.summary')}}</span>
-          </template>
-          <template #details>
-            {{$t('contact.waysToContact.phone.details[0]')}}
-            <a class="inline font-bold hover:text-blue-600" href="tel:+38631230167">{{$t('contact.waysToContact.phone.details[1]')}}</a>
-            {{$t('contact.waysToContact.phone.details[2]')}}
-          </template>
-        </Disclosure>
-      </li>
-      <li>
-        <Disclosure>
-          <template #summary>
-            <span>{{$t('contact.waysToContact.email.summary')}}</span>
-          </template>
-          <template #details>
-            {{$t('contact.waysToContact.email.details[0]')}}
-            <a class="font-bold hover:text-blue-600" href="mailto:info@gaia.si">{{$t('contact.waysToContact.email.details[1]')}}</a>
-          </template>
-        </Disclosure>
-      </li>
-      <li>
-        <Disclosure>
-          <template #summary>
-            <span>{{$t('contact.waysToContact.contactForm.summary')}}</span>
-          </template>
-          <template #details>
-            <p>{{$t('contact.waysToContact.contactForm.details')}}</p>
-            <ContactForm class="mt-4" />
-          </template>
-        </Disclosure>
-      </li>
+        <ul class="mt-3 space-y-2">
+          <li>
+            <Disclosure>
+              <template #summary>
+                <span>{{$t('contact.waysToContact.phone.summary')}}</span>
+              </template>
+              <template #details>
+                {{$t('contact.waysToContact.phone.details[0]')}}
+                <a class="inline font-bold hover:text-blue-600" href="tel:+38631230167">{{$t('contact.waysToContact.phone.details[1]')}}</a>
+                {{$t('contact.waysToContact.phone.details[2]')}}
+              </template>
+            </Disclosure>
+          </li>
+          <li>
+            <Disclosure>
+              <template #summary>
+                <span>{{$t('contact.waysToContact.email.summary')}}</span>
+              </template>
+              <template #details>
+                {{$t('contact.waysToContact.email.details[0]')}}
+                <a class="font-bold hover:text-blue-600" href="mailto:info@gaia.si">{{$t('contact.waysToContact.email.details[1]')}}</a>
+              </template>
+            </Disclosure>
+          </li>
+          <li>
+            <Disclosure>
+              <template #summary>
+                <span>{{$t('contact.waysToContact.contactForm.summary')}}</span>
+              </template>
+              <template #details>
+                <p>{{$t('contact.waysToContact.contactForm.details')}}</p>
+                <ContactForm class="mt-4" />
+              </template>
+            </Disclosure>
+          </li>
 
-    </ul>
+        </ul>
 
-    <p class="mt-5 max-w-2xl">
-      {{$t('contact.youWillProbablyReachElijan')}}
-    </p>
+        <p class="mt-5 max-w-2xl">
+          {{$t('contact.youWillProbablyReachElijan')}}
+        </p>
+      </div>
+
+      <img 
+      :src="appleBlossomsImage"
+      :alt="$t('imgAlts.appleBlossoms')"
+      class="hidden lg:block w-full mx-auto w-96 h-80 object-cover rounded-md"
+    />
+
+    </div>
 
   </PageWrapper>
 
