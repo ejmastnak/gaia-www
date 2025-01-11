96a7ad40b2ac631130a5414b8f277ecbbef77a30
Elijan Mastnak <git@ejmastnak.com>
2025-01-11 13:56:02 +0100

    chore(wip): temporarily disable SL locale
    In preparation for publishing to prod, since SL locale is full of
    placeholder translations at this point. SL locale is kept on a separate
    `sl` branch for the time being.

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,10 +1,10 @@
 import { createI18n } from "vue-i18n";
-import sl from "./locales/sl.json";
+import en from "./locales/en.json";
 
 const i18n = createI18n({
   locale: import.meta.env.VITE_DEFAULT_LOCALE,
   fallbackLocale: import.meta.env.VITE_FALLBACK_LOCALE,
-  messages: {sl}
+  messages: {en}
 })
 
 export default i18n
