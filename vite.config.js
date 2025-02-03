db4706c8fec2618980c1261e08213d069af2ea46
Elijan Mastnak <git@ejmastnak.com>
2025-02-03 20:41:12 +0100

    feat: add vite-plugin-image-optimizer to optimize hero images

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import { imagetools } from 'vite-imagetools'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   plugins: [
@@ -12,6 +13,10 @@ export default defineConfig({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**'),
     }),
     imagetools(),
+    ViteImageOptimizer({
+      // Optimize hero images
+      test: /hero-.*\.png$/i,
+    }),
   ],
   resolve: {
     alias: {
