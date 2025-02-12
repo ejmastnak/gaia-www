cb703e977a878c423e9e21370e88832b6940743c
Elijan Mastnak <git@ejmastnak.com>
2025-02-12 10:08:41 +0100

    style: give LanguageSwitcher a makeover

--- src/Components/LanguageSwitcher.vue
+++ src/Components/LanguageSwitcher.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { Listbox, ListboxButton, ListboxOptions, ListboxOption, } from '@headlessui/vue'
-import { ChevronDownIcon, GlobeAltIcon } from '@heroicons/vue/24/outline'
+import { Listbox, ListboxLabel, ListboxButton, ListboxOptions, ListboxOption, } from '@headlessui/vue'
+import { ChevronDownIcon, GlobeAltIcon, CheckIcon } from '@heroicons/vue/24/outline'
 import { useI18n } from 'vue-i18n'
 import { useRouter, useRoute } from "vue-router"
 import Translate from "@/i18n/translation"
@@ -39,34 +39,38 @@ const switchLanguageTo = async (newLocale) => {
 </script>
 
 <template>
-  <div class="relative">
-    <Listbox 
-      :modelValue="locale"
-      @update:modelValue="switchLanguageTo"
-    >
-      <ListboxButton class="flex items-center text-left bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 px-2 py-2">
-        <GlobeAltIcon class="w-6 h-6 translate-y-px text-gray-500 shrink-0" />
-        <span class="ml-1 uppercase text-lg font-bold">{{ locale }}</span>
-        <ChevronDownIcon class="translate-y-px ml-0.5 w-5 h-5 text-gray-500 shrink-0" />
-      </ListboxButton>
-      <ListboxOptions class="absolute right-0 mt-0.5 text-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500">
-        <ListboxOption
-          v-for="(option, idx) in supportedLocales"
-          :key="`locale-${option}`"
-          :value="option"
-          class="text-left cursor-pointer"
-          v-slot="{ active, selected }"
-        >
-          <li class="flex items-center px-4 py-1.5" :class="{
-            'bg-blue-500 text-white': active,
-            'text-gray-500': !selected && !active,
-            'font-bold': selected,
-          }"
-          >
-            {{ supportedLocaleNames[idx] }}
-          </li>
-        </ListboxOption>
-      </ListboxOptions>
+  <div class="relative w-fit">
+    <Listbox as="div" :modelValue="locale" @update:modelValue="switchLanguageTo">
+      <ListboxLabel class="sr-only">Change language</ListboxLabel>
+      <div class="relative">
+        <ListboxButton class="inline-flex divide-x divide-orange-700 rounded-md outline-none">
+          <div class="inline-flex items-center gap-x-1.5 rounded-l-md bg-orange-700 px-3 py-2 text-white">
+            <GlobeAltIcon class="-ml-0.5 size-5" aria-hidden="true" />
+            <p class="text-sm font-semibold">{{locale}}</p>
+          </div>
+          <div class="inline-flex items-center rounded-l-none rounded-r-md bg-orange-700 p-2 outline-none hover:bg-orange-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-400">
+            <span class="sr-only">Change language</span>
+            <ChevronDownIcon class="size-5 text-white forced-colors:text-[Highlight]" aria-hidden="true" />
+          </div>
+        </ListboxButton>
+
+        <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+          <ListboxOptions class="absolute right-0 z-10 mt-2 w-40 origin-top-right divide-y divide-gray-200 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+            <ListboxOption as="template" v-for="(option, idx) in supportedLocales" :key="`locale-${option}`" :value="option" v-slot="{ active, selected }">
+              <li :class="[active ? 'bg-orange-700 text-white' : 'text-gray-900', 'cursor-default select-none p-4 text-sm']">
+                <div class="flex flex-col">
+                  <div class="flex justify-between">
+                    <p :class="selected ? 'font-semibold' : 'font-normal'">{{ supportedLocaleNames[idx] }}</p>
+                    <span v-if="selected" :class="active ? 'text-white' : 'text-orange-600'">
+                      <CheckIcon class="size-5" aria-hidden="true" />
+                    </span>
+                  </div>
+                </div>
+              </li>
+            </ListboxOption>
+          </ListboxOptions>
+        </transition>
+      </div>
     </Listbox>
   </div>
 </template>
