782519df3c136f902511c411935384fcff15c9b3
Elijan Mastnak <git@ejmastnak.com>
2025-02-13 09:38:52 +0100

    style: tweak LanguageSwitcher colors

--- src/Components/LanguageSwitcher.vue
+++ src/Components/LanguageSwitcher.vue
@@ -43,12 +43,12 @@ const switchLanguageTo = async (newLocale) => {
     <Listbox as="div" :modelValue="locale" @update:modelValue="switchLanguageTo">
       <ListboxLabel class="sr-only">Change language</ListboxLabel>
       <div class="relative">
-        <ListboxButton class="inline-flex divide-x divide-orange-700 rounded-md outline-none">
-          <div class="inline-flex items-center gap-x-1.5 rounded-l-md bg-orange-700 px-3 py-2 text-white">
+        <ListboxButton class="inline-flex divide-x divide-orange-600 rounded-md outline-none">
+          <div class="inline-flex items-center gap-x-1.5 rounded-l-md bg-orange-600 px-3 py-2 text-white">
             <GlobeAltIcon class="-ml-0.5 size-5" aria-hidden="true" />
             <p class="text-sm font-semibold">{{locale}}</p>
           </div>
-          <div class="inline-flex items-center rounded-l-none rounded-r-md bg-orange-700 p-2 outline-none hover:bg-orange-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-400">
+          <div class="inline-flex items-center rounded-l-none rounded-r-md bg-orange-600 p-2 outline-none hover:bg-orange-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-400">
             <span class="sr-only">Change language</span>
             <ChevronDownIcon class="size-5 text-white forced-colors:text-[Highlight]" aria-hidden="true" />
           </div>
@@ -57,7 +57,7 @@ const switchLanguageTo = async (newLocale) => {
         <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
           <ListboxOptions class="absolute right-0 z-10 mt-2 w-40 origin-top-right divide-y divide-gray-200 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
             <ListboxOption as="template" v-for="(option, idx) in supportedLocales" :key="`locale-${option}`" :value="option" v-slot="{ active, selected }">
-              <li :class="[active ? 'bg-orange-700 text-white' : 'text-gray-900', 'cursor-default select-none p-4 text-sm']">
+              <li :class="[active ? 'bg-orange-600 text-white' : 'text-gray-900', 'cursor-default select-none p-4 text-base']">
                 <div class="flex flex-col">
                   <div class="flex justify-between">
                     <p :class="selected ? 'font-semibold' : 'font-normal'">{{ supportedLocaleNames[idx] }}</p>
