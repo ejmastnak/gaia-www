6327835509c8706778fd6034e82c3fe17dfbe941
Elijan Mastnak <git@ejmastnak.com>
2025-04-16 14:06:02 +0200

    chore: move Orchard page content to i18n keys

--- src/Views/Orchard.vue
+++ src/Views/Orchard.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 import I18nRouterLink from '@/Components/I18nRouterLink.vue'
 import PageWrapper from '@/Shared/PageWrapper.vue'
+import SimpleImagePanel from '@/Components/SimpleImagePanel.vue'
 import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/vue/24/outline'
 
 import appleBlossomsImage from '@/assets/img/apple-blossoms.jpg?w=600'
@@ -11,22 +12,85 @@ import appleBlossomsImage from '@/assets/img/apple-blossoms.jpg?w=600'
 <template>
 
   <PageWrapper>
-    <h1 class="text-5xl font-['Latin_Modern_Roman']">Travniški sadovnjak</h1>
+    <h1 class="text-5xl font-['Latin_Modern_Roman']">{{$t('orchard.h1')}}</h1>
 
-    <!-- -mr moves image rightward -->
-    <div class="mt-10 lg:flex space-x-16 lg:-mr-12">
-      <div>
-        <p>Travniški sadovnjak je...</p>
-        <p class="mt-2">Značilonsti travniškega sadovnjaka...</p>
-      </div>
+    <p class="mt-10 max-w-3xl">
+      {{$t('orchard.intro[0]')}}
+      <span class="italic">{{$t('orchard.intro[1]')}}</span> 
+      {{$t('orchard.intro[2]')}}
+      <span class="italic">{{$t('orchard.intro[3]')}}</span>
+      <span class="">{{$t('orchard.intro[4]')}}</span>
+    </p>
 
-      <img 
-      :src="appleBlossomsImage"
-      :alt="$t('imgAlts.appleBlossoms')"
-      class="hidden lg:block mx-auto !w-96 h-80 object-cover rounded-md"
-    />
+    <SimpleImagePanel 
+      class="mt-5 md:mt-16"
+      :reverse="false"
+      :imgFile="appleBlossomsImage"
+      :imgAlt="$t('imgAlts.appleBlossoms')"
+    >
+      <p>{{$t('orchard.definition')}}</p>
+      <p class="mt-3">{{$t('orchard.features')}}</p>
+    </SimpleImagePanel>
 
-    </div>
+    <p class="mt-5 md:mt-12 max-w-3xl">
+      {{$t('orchard.mosiac')}}
+    </p>
+
+    <SimpleImagePanel 
+      class="mt-5 md:mt-16"
+      :reverse="true"
+      :imgFile="appleBlossomsImage"
+      :imgAlt="$t('imgAlts.appleBlossoms')"
+    >
+      <p class="mt-3">
+        <span class="font-bold">{{$t('orchard.comparisonTitle')}}</span>
+        {{$t('orchard.comparison')}}
+      </p>
+    </SimpleImagePanel>
+
+    <p class="mt-5">{{$t('orchard.notesHeading')}}</p>
+
+    <ul class="mt-5 ml-5 list-disc max-w-3xl space-y-3">
+      <li>
+        {{$t('orchard.notes.species.intro')}}
+        <a href="https://en.wikipedia.org/wiki/Apple" class="text-blue-600 hover:text-blue-700 hover:underline">{{$t('orchard.notes.species.apple')}}</a>
+        <a href="https://en.wikipedia.org/wiki/Malus_sylvestris" class="text-blue-600 hover:text-blue-700 hover:underline">{{$t('orchard.notes.species.crabapple')}}</a>
+        <a href="https://en.wikipedia.org/wiki/Pyrus_communis" class="text-blue-600 hover:text-blue-700 hover:underline">{{$t('orchard.notes.species.pear')}}</a>
+        <a href="https://en.wikipedia.org/wiki/Prunus_domestica" class="text-blue-600 hover:text-blue-700 hover:underline">{{$t('orchard.notes.species.plum')}}</a>
+        <a href="https://en.wikipedia.org/wiki/Prunus_avium" class="text-blue-600 hover:text-blue-700 hover:underline">{{$t('orchard.notes.species.sweetCherry')}}</a>
+        <a href="https://en.wikipedia.org/wiki/Prunus_cerasus" class="text-blue-600 hover:text-blue-700 hover:underline">{{$t('orchard.notes.species.sourCherry')}}</a>
+        {{$t('orchard.notes.species.and')}}
+        <a href="https://en.wikipedia.org/wiki/Juglans_regia" class="text-blue-600 hover:text-blue-700 hover:underline">{{$t('orchard.notes.species.walnut')}}</a>
+      </li>
+
+        <!-- https://anediblelandscape.wordpress.com/the-traditional-orchard-story/ -->
+      <li>{{$t('orchard.notes.integrated')}}</li>
+
+      <li>
+        <p>
+          {{$t('orchard.notes.biodiversity.p1')}}
+        </p>
+
+        <p class="mt-2">
+          {{$t('orchard.notes.biodiversity.studies.intro')}}
+          <a href="https://doi.org/10.1016/j.biocon.2024.110815" class="hover:text-blue-700 hover:underline">
+            {{$t('orchard.notes.biodiversity.studies.guariento.authors')}}
+            <span class="italic">{{$t('orchard.notes.biodiversity.studies.guariento.title')}}</span>
+            {{$t('orchard.notes.biodiversity.studies.guariento.journal')}}
+          </a>
+          <a href="https://doi.org/10.1016/j.actao.2016.12.010" class="hover:text-blue-700 hover:underline">
+            {{$t('orchard.notes.biodiversity.studies.kajtoch.authors')}}
+            <span class="italic">{{$t('orchard.notes.biodiversity.studies.kajtoch.title')}}</span>
+            {{$t('orchard.notes.biodiversity.studies.kajtoch.journal')}}
+          </a>
+          {{$t('orchard.notes.biodiversity.studies.andReferencesTherein')}}
+        </p>
+
+      </li>
+
+    </ul>
+
+
 
   </PageWrapper>
 
---
+++ src/i18n/translations/orchard.cjs
@@ -0,0 +1,142 @@
+module.exports = {
+  h1: {
+    en: "Meadow orchards",
+    sl: "Travniški sadovnjak",
+  },
+  intro: [
+    {
+      en: "Our farm consists primarily of",
+      sl: "Our farm consists primarily of",
+    },
+    {
+      en: "meadow orchards",
+      sl: "meadow orchards",
+    },
+    {
+      en: "(sometimes also translated from Slovene as ",
+      sl: "(sometimes also translated from Slovene as ",
+    },
+    {
+      en: "tall-trunk meadow orchards",
+      sl: "tall-trunk meadow orchards",
+    },
+    {
+      en: "). Our visitors and customers often ask what is a meadow orchard, since the habitat is rarely found outside of the farming countryside. Here is our description. ",
+      sl: "). Our visitors and customers often ask what is a meadow orchard, since the habitat is rarely found outside of the farming countryside. Here is our description. ",
+    },
+  ],
+  definition: {
+    en: `Meadow orchards are a traditional agroforestry system characterized by high-growing, old-growth fruit trees scattered sparsely and irregularly throughout a grassland understory. In the name "tall-trunk meadow orchard", the meadow refers to the grassland understory, orchard to the fruit trees, and "tall-trunk" to the tree's high-growing, tall trunks (in contrast to the short trees found in conventional intensive orchards).`,
+    sl: `Meadow orchards are a traditional agroforestry system characterized by high-growing, old-growth fruit trees scattered sparsely and irregularly throughout a grassland understory. In the name "tall-trunk meadow orchard", the meadow refers to the grassland understory, orchard to the fruit trees, and "tall-trunk" to the tree's high-growing, tall trunks (in contrast to the short trees found in conventional intensive orchards).`,
+  },
+  features: {
+    en: "Meadow orchards consist of mixed species of fruit trees, often grafted onto wild stock, and the understory is traditionally grazed by free-ranging livestock or mowed for use as hay.",
+    sl: "Meadow orchards consist of mixed species of fruit trees, often grafted onto wild stock, and the understory is traditionally grazed by free-ranging livestock or mowed for use as hay.",
+  },
+  mosiac: {
+    en: "Meadow orchards are a defining feature in the mosaic of extensively-managed pastures, crop fields, hedges, and orchards characterizing the traditional farming countryside of temperate Europe, allow for integrated fruit production and grazing in otherwise marginal land, and provide valuable ecological habitat for flowers, grasses, insects, and birds.",
+    sl: "Meadow orchards are a defining feature in the mosaic of extensively-managed pastures, crop fields, hedges, and orchards characterizing the traditional farming countryside of temperate Europe, allow for integrated fruit production and grazing in otherwise marginal land, and provide valuable ecological habitat for flowers, grasses, insects, and birds.",
+  },
+  comparisonTitle: {
+    en: "Comparison to conventional orchards:",
+    sl: "Comparison to conventional orchards:",
+  },
+  comparison: {
+    en: "the tall, old-growth trees, mixed varieties, and scattered distribution of trees in meadow orchards contrasts with conventional fruit orchards, which are characterized by monoculture plantations of short, heavily-pruned trees closely spaced in regular rows—the short trees and regular spacing in conventional orchards allow for efficient chemical treatments and mechanized harvest. Intensive orchards conventionally lack an appreciable meadow undestory and rely heavily on agrochemical application to sustain high yields. ",
+    sl: "the tall, old-growth trees, mixed varieties, and scattered distribution of trees in meadow orchards contrasts with conventional fruit orchards, which are characterized by monoculture plantations of short, heavily-pruned trees closely spaced in regular rows—the short trees and regular spacing in conventional orchards allow for efficient chemical treatments and mechanized harvest. Intensive orchards conventionally lack an appreciable meadow undestory and rely heavily on agrochemical application to sustain high yields. ",
+  },
+  notesHeading: {
+    en: "Some notes:",
+    sl: "Some notes:",
+  },
+  notes: {
+    species: {
+      intro: {
+        en: "Fruit and nut trees commonly found in meadow orchards in our native Koroška region include many varieties of",
+        sl: "Fruit and nut trees commonly found in meadow orchards in our native Koroška region include many varieties of",
+      },
+      apple: {
+        en: "apple (Malus domestica), ",
+        sl: "apple (Malus domestica), ",
+      },
+      crabapple: {
+        en: "crabapple (Malus sylvestris), ",
+        sl: "crabapple (Malus sylvestris), ",
+      },
+      pear: {
+        en: "pear (Pyrus communis), ",
+        sl: "pear (Pyrus communis), ",
+      },
+      plum: {
+        en: "plum (Prunus domestica), ",
+        sl: "plum (Prunus domestica), ",
+      },
+      sweetCherry: {
+        en: "sweet cherry (Prunus avium), ",
+        sl: "sweet cherry (Prunus avium), ",
+      },
+      sourCherry: {
+        en: "sour cherry (Prunus cerasus), ",
+        sl: "sour cherry (Prunus cerasus), ",
+      },
+      and: {
+        en: "and ",
+        sl: "and ",
+      },
+      walnut: {
+        en: "common walnut (Juglans regia).",
+        sl: "common walnut (Juglans regia).",
+      },
+    },
+    integrated: {
+      en: "Meadow orchards have traditional significance as a form of integrated land management that produces both fruit and feed for livestock in marginal, steep, and otherwise difficult-to-cultivate land. Historically, the fruit provided both food for family use and a source of supplemental income when sold in market.",
+      sl: "Meadow orchards have traditional significance as a form of integrated land management that produces both fruit and feed for livestock in marginal, steep, and otherwise difficult-to-cultivate land. Historically, the fruit provided both food for family use and a source of supplemental income when sold in market.",
+    },
+    biodiversity: {
+      p1: {
+        en: "Meadow orchards have tremendous ecological value. The tall, often hollow trunks and large crowns offer rare nesting and roosting sites to hollow-dwelling birds and bats. Fungi, mosses, and lichens grow on the older trees, and the mixed grassland understory and fruit trees provide foraging sites and living habitat for insects, birds, small mammals, and reptiles. Honey bees, wild bees, bumblebees, and numerous other pollinators visit the flowers for nectar, and in turn contribute beneficially to pollination throughout the surrounding farmland; and the diverse populations of insects and birds help with pest control.",
+        sl: "Meadow orchards have tremendous ecological value. The tall, often hollow trunks and large crowns offer rare nesting and roosting sites to hollow-dwelling birds and bats. Fungi, mosses, and lichens grow on the older trees, and the mixed grassland understory and fruit trees provide foraging sites and living habitat for insects, birds, small mammals, and reptiles. Honey bees, wild bees, bumblebees, and numerous other pollinators visit the flowers for nectar, and in turn contribute beneficially to pollination throughout the surrounding farmland; and the diverse populations of insects and birds help with pest control.",
+      },
+      studies: {
+        intro: {
+          en: "Numerous studies have investigated and confirmed high biodiversity in meadow orchards; see e.g.",
+          sl: "Numerous studies have investigated and confirmed high biodiversity in meadow orchards; see e.g.",
+        },
+        guariento: {
+          authors: {
+            en: "Guariento, E., et. al, 2024.",
+            sl: "Guariento, E., et. al, 2024.",
+          },
+          title: {
+            en: "Meadow orchards as a good practice example for improving biodiversity in intensive apple orchards.",
+            sl: "Meadow orchards as a good practice example for improving biodiversity in intensive apple orchards.",
+          },
+          journal: {
+            en: "Biological Conservation, Volume 299, ",
+            sl: "Biological Conservation, Volume 299, ",
+          },
+        },
+        //  , and the references therein. 
+        kajtoch: {
+          authors: {
+            en: "Kajtoch, Ł., 2017",
+            sl: "Kajtoch, Ł., 2017",
+          },
+          title: {
+            en: "The importance of traditional orchards for breeding birds: the preliminary study on central European example.",
+            sl: "The importance of traditional orchards for breeding birds: the preliminary study on central European example.",
+          },
+          journal: {
+            en: "Acta Oecol. 78, 53–60, ",
+            sl: "Acta Oecol. 78, 53–60 ",
+          },
+        },
+        andReferencesTherein: {
+          en: "and the references therein.",
+          sl: "and the references therein.",
+        },
+
+      }
+    },
+  }
+};
