40a6de35a73b1ca8f104541ebc0d96ec95d4a485
Elijan Mastnak <git@ejmastnak.com>
2025-05-03 15:53:23 +0200

    style: tweak button spacing

--- src/Shared/Bio.vue
+++ src/Shared/Bio.vue
@@ -6,7 +6,7 @@ import SecondaryLink from '@/Components/SecondaryLink.vue'
   <div class="p-2 px-3 text-base border border-green-700 bg-green-50 rounded">
     <p class="font-bold text-green-800">{{$t('shared.bio.heading')}}</p>
     <p>{{$t('shared.bio.text')}}</p>
-    <SecondaryLink class="mt-2" routeBasename="bio">
+    <SecondaryLink class="mt-2 !px-2.5 !py-1.5" routeBasename="bio">
       {{$t("partials.readMore")}}
       <span aria-hidden="true"> &rarr;</span>
     </SecondaryLink>
